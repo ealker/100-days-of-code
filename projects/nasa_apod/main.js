a2a08a56aa5a3474945ae38d5c511993be7c9979
Elliot Alker <elliot@anamatica.com>
2020-02-05 00:29:49 +0000

    Cleaned up code. Finished with this project for now. May revisit later.

--- projects/nasa_apod/main.js
+++ projects/nasa_apod/main.js
@@ -1,7 +1,14 @@
-document.getElementById('getDataButton').addEventListener('click', getData);
+// Event Handler called when the form has been submitted. 
+$("form").submit(function() {
+    event.preventDefault();
+    let date = $("input[name='date']").data('datepicker').getFormattedDate('yyyy-mm-dd');
+    getData(date);
+});
 
-function getData() {
-    fetch('https://api.nasa.gov/planetary/apod?date=2019-07-11&api_key=DEMO_KEY')
+function getData(date) {
+    let request_url = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=' + date.toString() 
+    console.log(request_url);
+    fetch(request_url)
         .then(
             function (response) {
                 if (response.status !== 200) {
@@ -12,16 +19,17 @@ function getData() {
                 // Examine the text in the response
                 response.json().then(function (data) {
                     console.log(data);
+                    console.log(data.url);
                     const name = data.title
-                    const date = data.date
+                    const date_taken = data.date
                     const copyright = data.copyright
                     const description = data.explanation
                     const picture_url = data.url
 
-                    document.getElementById("name").append(name);
-                    document.getElementById("date").append(date);
-                    document.getElementById("copyright").append(copyright);
-                    document.getElementById("description").append(description);
+                    document.getElementById("name").innerHTML = "Name: " + name;
+                    document.getElementById("date-taken").innerHTML = "Date Taken: " + date_taken;
+                    document.getElementById("copyright").innerHTML = "Copyright: " + copyright;
+                    document.getElementById("description").innerHTML = "Image Description: " + description;
                     document.getElementById("picture").src = picture_url;
                 });
             }
@@ -61,4 +69,3 @@ function getTodayImage() {
             console.log('Fetch Error :-S', err);
         });
 }
-
